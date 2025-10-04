457cab80661615a3d85c7bb72a6bd75b2f8a5688
Александр Сергиенко <s_sandr06@mail.ru>
2025-10-04 15:28:46 +0300

    В p2 скопирован build.gradle.kts

--- project/practise-2/build.gradle.kts
+++ project/practise-2/build.gradle.kts
@@ -1,25 +1,45 @@
 plugins {
-    id("java")
+    java
+    checkstyle
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "studying"
-version = "1.0-SNAPSHOT"
+group = "hse"
+version = "0.0.1-SNAPSHOT"
+
+checkstyle {
+    toolVersion = "10.13.0"
+    isIgnoreFailures = false
+    maxWarnings = 0
+    maxErrors = 0
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.36")
-    annotationProcessor("org.projectlombok:lombok:1.18.36")
-
-    testCompileOnly("org.projectlombok:lombok:1.18.36")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
-
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
