5d3455795d0c85641bdebcd02f756138dafbb669
Александр Сергиенко <s_sandr06@mail.ru>
2025-10-04 15:56:17 +0300

    Добавлен поясняющий комментарий

--- project/practise-3/src/test/java/hse/kpo/KpoApplicationTests.java
+++ project/practise-3/src/test/java/hse/kpo/KpoApplicationTests.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Интеграционный тест
 @SpringBootTest
 class KpoApplicationTests {
 
