8cadd5603ee6eadf94b409452c5df0212ac04663
Александр Сергиенко <s_sandr06@mail.ru>
2025-10-04 15:56:43 +0300

    Форматирование файла с тестами

--- project/practise-3/src/test/java/hse/kpo/KpoApplicationTests.java
+++ project/practise-3/src/test/java/hse/kpo/KpoApplicationTests.java
@@ -18,48 +18,48 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KpoApplicationTests {
 
-	@Autowired
-	private CarService carService;
+    @Autowired
+    private CarService carService;
 
-	@Autowired
-	private CustomerStorage customerStorage;
+    @Autowired
+    private CustomerStorage customerStorage;
 
-	@Autowired
-	private HseCarService hseCarService;
+    @Autowired
+    private HseCarService hseCarService;
 
-	@Autowired
-	private PedalCarFactory pedalCarFactory;
+    @Autowired
+    private PedalCarFactory pedalCarFactory;
 
-	@Autowired
-	private HandCarFactory handCarFactory;
+    @Autowired
+    private HandCarFactory handCarFactory;
 
-	@Test
-	@DisplayName("Тест загрузки контекста")
-	void contextLoads() {
-		Assertions.assertNotNull(carService);
-		Assertions.assertNotNull(customerStorage);
-		Assertions.assertNotNull(hseCarService);
-	}
+    @Test
+    @DisplayName("Тест загрузки контекста")
+    void contextLoads() {
+        Assertions.assertNotNull(carService);
+        Assertions.assertNotNull(customerStorage);
+        Assertions.assertNotNull(hseCarService);
+    }
 
-	@Test
-	@DisplayName("Тест загрузки контекста")
-	void hseCarServiceTest() {
-		customerStorage.addCustomer(new Customer("Ivan1",6,4));
-		customerStorage.addCustomer(new Customer("Maksim",4,6));
-		customerStorage.addCustomer(new Customer("Petya",6,6));
-		customerStorage.addCustomer(new Customer("Nikita",4,4));
-		
-		carService.addCar(pedalCarFactory, new PedalEngineParams(6));
-		carService.addCar(pedalCarFactory, new PedalEngineParams(6));
+    @Test
+    @DisplayName("Тест загрузки контекста")
+    void hseCarServiceTest() {
+        customerStorage.addCustomer(new Customer("Ivan1", 6, 4));
+        customerStorage.addCustomer(new Customer("Maksim", 4, 6));
+        customerStorage.addCustomer(new Customer("Petya", 6, 6));
+        customerStorage.addCustomer(new Customer("Nikita", 4, 4));
 
-		carService.addCar(handCarFactory, EmptyEngineParams.DEFAULT);
-		carService.addCar(handCarFactory, EmptyEngineParams.DEFAULT);
+        carService.addCar(pedalCarFactory, new PedalEngineParams(6));
+        carService.addCar(pedalCarFactory, new PedalEngineParams(6));
 
-		customerStorage.getCustomers().stream().map(Customer::toString).forEach(System.out::println);
+        carService.addCar(handCarFactory, EmptyEngineParams.DEFAULT);
+        carService.addCar(handCarFactory, EmptyEngineParams.DEFAULT);
 
-		hseCarService.sellCars();
+        customerStorage.getCustomers().stream().map(Customer::toString).forEach(System.out::println);
 
-		customerStorage.getCustomers().stream().map(Customer::toString).forEach(System.out::println);
-	}
+        hseCarService.sellCars();
+
+        customerStorage.getCustomers().stream().map(Customer::toString).forEach(System.out::println);
+    }
 
 }
